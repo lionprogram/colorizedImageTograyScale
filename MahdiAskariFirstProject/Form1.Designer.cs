d77b1de649e2ae805e1693569ab2036e97f91bfa
lionprogram <mahdi6269@gmail.com>
2023-11-13 10:50:01 -0800

    project complete

---
+++ MahdiAskariFirstProject/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MahdiAskariFirstProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("2  Koodak", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.Location = new System.Drawing.Point(400, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "تبدیل";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Koodak", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button2.Location = new System.Drawing.Point(259, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "انتخاب تصویر";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(451, 396);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(469, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(451, 396);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 414);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 28);
+            this.textBox1.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 448);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(241, 25);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(540, 414);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(380, 59);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 485);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "تبدیل تصویر رنگی به سیاه سفید";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ MahdiAskariFirstProject/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MahdiAskariFirstProject
+{
+    public partial class Form1 : Form
+    {
+        Bitmap RowPic;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Img_Open = new OpenFileDialog();
+            Img_Open.Filter = "Image File(*.Gif; *.jpg)| *.Gif; *.jpg";
+            Img_Open.Title = "بارگذاری تصویر";
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (Img_Open.ShowDialog() == DialogResult.OK) { 
+
+                pictureBox1.Image = Image.FromFile(Img_Open.FileName);
+                RowPic = new Bitmap(pictureBox1.Image);
+                textBox1.Text += "width: "+Convert.ToString(RowPic.Width)+" - height: "+ Convert.ToString(RowPic.Height);
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Bitmap result = new Bitmap(pictureBox1.Image);
+            Color pix1, pix2;
+            int r, g, b, intensity;
+            for (int x = 0; x <= RowPic.Width-1; x++)
+            {
+                for (int y = 0; y <= RowPic.Height-1 ; y++)
+                {
+                    pix1 = RowPic.GetPixel(x,y);
+                    r = pix1.R;
+                    g = pix1.G;
+                    b = pix1.B;
+                    intensity = (r + g + b) / 3;
+                    pix2 = Color.FromArgb(intensity,intensity,intensity);
+                    result.SetPixel(x,y,pix2);
+                    
+                }
+                    pictureBox2.Image = result;
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int XPoint = Cursor.Position.X;
+            int YPoint = Cursor.Position.Y;
+            int r, g, b;
+            r = RowPic.GetPixel(XPoint, YPoint).R;
+            g = RowPic.GetPixel(XPoint, YPoint).G;
+            b = RowPic.GetPixel(XPoint, YPoint).B;
+            richTextBox1.Text = "RED: "+r+" - GREEN: "+g+" - BLUE: "+b;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            int XPoint = Cursor.Position.X;
+            int YPoint = Cursor.Position.Y;
+            int r, g, b;
+            r = RowPic.GetPixel(XPoint, YPoint).R;
+            g = RowPic.GetPixel(XPoint, YPoint).G;
+            b = RowPic.GetPixel(XPoint, YPoint).B;
+            richTextBox2.Text = "RED: " + r + " - GREEN: " + g + " - BLUE: " + b;
+        }
+    }
+}
